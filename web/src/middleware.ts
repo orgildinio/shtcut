a00db7a4550992baab501ce11be80f33753153a8
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2025-01-06 23:05:38 +0100

    update on deployment bug

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -31,14 +31,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    unstable_allowDynamic: [
-        '**/node_modules/function-bind/**',
-        '/node_modules/lodash/_root.js',
-        '/node_modules/lodash/_cloneBuffer.js',
-        '/node_modules/lodash/_baseClone.js',
-        '/node_modules/lodash/omit.js',
-        '/src/redux/selectors/user/index.ts',
-        '/src/hooks/user/index.ts',
-        '/src/hooks/index.ts'
-    ]
+    unstable_allowDynamic: ['**/node_modules/function-bind/**']
 };
