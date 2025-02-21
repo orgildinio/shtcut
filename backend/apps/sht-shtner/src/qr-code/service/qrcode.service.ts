9fef1ee975b533582e3a7c5eddb312227e839bd2
imodoye <imodoye.david@mtn.com>
2025-02-21 03:01:55 +0100

    code refactoring

--- backend/apps/sht-shtner/src/qr-code/service/qrcode.service.ts
+++ backend/apps/sht-shtner/src/qr-code/service/qrcode.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import lang from 'apps/sht-shtner/lang';
 import { ClientSession, Model } from 'mongoose';
